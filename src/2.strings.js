f68b984a95cd6e14860a9541086187d68f6106eb
kooncewc <kooncewc@mail.uc.edu>
2022-05-22 18:56:07 -0400

    Updated string.js
    String.js completed

--- src/2.strings.js
+++ src/2.strings.js
@@ -9,18 +9,23 @@ function reverseString(str) {
    * 
    * Can even be done on one line
    */
-  return ;
+  var Stringsplit = str.split("");
+  var reverseArray = Stringsplit.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
 }
 
 //capitalize: should return the input in all-caps
 // Example: "this is a string" => "THIS IS A STRING"
 function capitalize(str) {
+  str.value = str.value.toLocaleUpperCase();
   return ;
 }
 
 // splitString: should divide a string into substrings and return an array
 // Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
 function splitString(str, splitAt = `,`) {
+  var splitString = str.split("");
   return ;
 }
 module.exports = {
